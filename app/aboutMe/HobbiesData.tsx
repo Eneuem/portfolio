20fa7555efb1a11d21e74880af47033b4e470232
Eneuem <nemozor@gmail.com>
2024-03-05 16:09:53 +0100

    yo

--- app/aboutMe/HobbiesData.tsx
+++ app/aboutMe/HobbiesData.tsx
@@ -1,5 +1,5 @@
 const HobbiesData = {
-    "hobbies": ["I've been a rapper for years", "Launched a lot of small buisnesses", "I can eat a burger a day", "I went to New-Zealand for 5 months", "My favorite movie is Inglorious Basterds", "I play rugby since I m 7 years old" ],
+    "hobbies": ["I've been a rapper for years", "Launched a lot of small buisnesses", "I can eat a burger a day", "I went to New-Zealand for 5 months", "My favorite movie is Inglorious Basterds", "I play rugby since I m 7 years old", "I did my first website at 14 y/o", "I studied law for 3 years" ],
 }
 
 export default HobbiesData;
